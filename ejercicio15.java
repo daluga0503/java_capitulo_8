9e53b28295bf1e962583f1cf30c011ad97971cba
daluga0503 <daniel.luquegallardo@gmail.com>
2022-12-07 13:10:30 +0100

    15-16 added

---
+++ ejercicio15.java
@@ -0,0 +1,10 @@
+
+public class ejercicio15 {
+    public static void main(String[] args) {
+        for(int i = 2; i<1000; i++){
+            if(matematicas.funciones.esPrimo(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
---
+++ ejercicio16.java
@@ -0,0 +1,9 @@
+public class ejercicio16 {
+    public static void main(String[] args) {
+        for(int i = 1; i<10000; i++){
+            if(matematicas.funciones.esCapicua(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
